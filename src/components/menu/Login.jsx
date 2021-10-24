13daa989e46c400b43716b5ce77fd698baba9cb6
RocioSulca <rociosulca.z@gmail.com>
2021-10-23 21:50:24 -0500

    Add styles, login and product

--- src/components/menu/Login.jsx
+++ src/components/menu/Login.jsx
@@ -1,28 +1,48 @@
-// import React, { useState } from "react";
-// import Logueo from "../pages/login";
-// import Home from '../pages/home'
-// import app from "../../fb-config";
-// import { getAuth, onAuthStateChanged } from "firebase/auth";
-// const auth = getAuth(app);
+import React, { useState, useEffect } from "react";
+import Logueo from "../pages/Logueo";
+import Home from '../pages/home'
+import app from "../../fb-config";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-// function Login() {
-//   const [usuarioGlobal, setUsuarioGlobal] = useState(null);
+const auth = getAuth(app);
 
-//   // onAuthStateChanged(auth, (usuarioFirebase) => {
-//   //   if (usuarioFirebase) {
-//   //     //código en caso de que haya sesión inciiada
-//   //     setUsuarioGlobal(usuarioFirebase);
-//   //   } else {
-//   //     //código en caso de que no haya sesión iniciada
-//   //     setUsuarioGlobal(null);
-//   //   }
-//   // });
+function Login() {
+  const [usuarioGlobal, setUsuarioGlobal] = useState(null);
 
-//   return (
-//     <>
-//       {usuarioGlobal ? (<Home />) : (<Logueo />)}
-//     </>
-//   );
-// }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (usuarioFirebase) => {
+      if (usuarioFirebase) {
+        //código en caso de que haya sesión inciiada
+        setUsuarioGlobal(usuarioFirebase);
+      } else {
+        //código en caso de que no haya sesión iniciada
+        setUsuarioGlobal(null);
+      }
+    });
+    return () => {
+      unsubscribe();
+    }
+  },[])
 
-// export default Login;
+//   onAuthStateChanged(auth, (usuarioFirebase) => {
+//     if (usuarioFirebase) {
+//       //código en caso de que haya sesión inciiada
+//       setUsuarioGlobal(usuarioFirebase);
+//     } else {
+//       //código en caso de que no haya sesión iniciada
+//       setUsuarioGlobal(null);
+//     }
+//   });
+
+  return (
+    <>
+      {usuarioGlobal ? (
+        <Home correoUsuario={usuarioGlobal.email} />
+      ) : (
+        <Logueo />
+      )}
+    </>
+  );
+}
+
+export default Login;
