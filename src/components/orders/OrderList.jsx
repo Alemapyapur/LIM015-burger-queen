cbfcd1eea9c7d549ddd59048aad1bc9a34ee3e18
Alemap Yapur <pamela.rupay31@gmail.com>
2021-11-01 18:16:16 -0500

    add card status to kitchen

--- src/components/orders/OrderList.jsx
+++ src/components/orders/OrderList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { db } from "../../fb-config";
 import { collection, getDocs, query, where } from "firebase/firestore";
-import AddProducts from './addProducts'
+// import AddProducts from './addProducts'
 import "../../styles/pages/waiter.css";
 
 
@@ -30,27 +30,63 @@ export default function OrderList({ setPedido, pedido }) {
   }, [pending]);
 
   return (
-    <section className="section-side-right">
-      <section className="btn-order">
-        <button className="btn-order-waiter" onClick={() => setPending('pendiente')}>PEDIDOS POR ENTREGAR</button>
-        <button className="btn-order-waiter" onClick={() => setPending('delivery')}>ENTREGADOS</button>
+    <section className="kitchen-view">
+      <section className="btn-order-kitchen">
+        <button className="btn-order-kitchen-delivery" onClick={() => setPending('pending')}>PEDIDOS POR ENTREGAR</button>
+        <button className="btn-order-kitchen-send" onClick={() => setPending('delivery')}>PEDIDOS ENTREGADOS</button>
       </section>
 
-      <div className="order-container-list">
+      <div>
         {arrayOrderList.map((item, index) => {
           //console.log('type', type);
           const productLists = {
-            name:item.name,
+            id:item.id,
+            name: item.name,
             order: item.order,
+            table: item.table,
             status: item.status
           }
           return (
-            <div className="order-container" >
+            <section className="card-order">
+              <section className="card-order-container">
+              <p className="nombre-kitchen">Cliente: {item.name}</p>
+              {/* <p className="text-order">Hora: {item.timestamp}</p> */}
+              <p className="hora-kitchen"> Mesa: {item.table}</p>
+              <p className="text-order">Status: {item.status}</p>
+            {/* <div className="order-container" >
               <p className="text-order">{item.name}</p>
               <p className="img-product" alt='img-burguerqueen' src={item.order}></p>
               <p className="text-order">{item.status}</p>
                 <section><AddProducts productList={productLists}  /></section>
-              </div>
+              </div> */}
+
+                <section className="kitchen-box">
+                <table className="kitchen-box-table">
+                <thead className="kitchen-box-table-title">
+                  <tr className="table-kitchen-titles">
+                    <th className="table-kitchen-title1">Cantidad</th>
+                    <th className="table-kitchen-title2">Nombre</th>
+                    <th className="table-kitchen-title3">Price</th>
+                  </tr>
+                </thead>
+
+                <tbody className="table-kitchen">
+                <tr className="table-kitchen-order">
+                    <td className="table-kitchen-product">2</td>
+                    <td className="table-kitchen-product">Cafe Americano</td>
+                    <td className="table-kitchen-product">S/ 15.00</td>
+                </tr>
+                </tbody>
+                </table>
+                <section className="table-kitchen-total">
+                TOTAL: S/ {item.order}
+                </section>
+                <button className="btn-kitchen-send-order" productList={productLists} >
+                  LISTO PARA ENTREGAR
+                </button>
+                </section>
+              </section>
+            </section>
           );
         })}
       </div>
